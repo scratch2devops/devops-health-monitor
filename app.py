7065d1288a45ea831217e42ee3dd6b0f1fe7588e
Swathika <swathika.janardhan@gmail.com>
2026-09-23 23:35:57 +0530

    adding webserver Framework for building Web application

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-import time
+from flask import Flask
 import psutil
-while True:
-    print("DevOps Health Monitor is running!")
-    print("CPU Usage:", psutil.cpu_percent(), "%")
-    print("Memory Usage:", psutil.virtual_memory().percent, "%")
 
-    time.sleep(5)
+app = Flask(__name__)
+
+@app.route("/")
+def health():
+    cpu = psutil.cpu_percent()
+    memory = psutil.virtual_memory().percent
+
+    return f"""
+    <h1>DevOps Health Monitor</h1>
+    <p>CPU Usage: {cpu}%</p>
+    <p>Memory Usage: {memory}%</p>
+    """
+
+app.run(host="0.0.0.0", port=8000)
